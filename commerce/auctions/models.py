568fdcf220958516c966a20e442f90508fd9f08c
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-14 20:41:35 -0300

    Update models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,31 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+import Pillow
 
 class User(AbstractUser):
     pass
 
 #Properties  register
-class Property(models.Model):
+class ActiveListings(models.Model):
     Pcode = models.CharField(max_length=5) #key
-    type = models.CharField(help_text="House, Apartament, Commercial",max_length=2) #foreign key from PropTypes
-    size = models.DecimalField(max_digits=6, decimal_places=2) #m**2
+    Ccode=models.IntegerField(help_text="Category House, Apartament, Commercial") #foreign key from PropTypes
     description = models.CharField(max_length=250)
-    picture = models.CharField(max_length=60) #Name of the picture saved on static folder
+    picture = models.ImageField() #Name of the picture saved on static folder
     price = models.DecimalField(help_text="Just USD", max_digits=9, decimal_places=2) #initial price
     def __str__(self):
-        return f"({self.Pcode}) ({self.type}) ({self.size}) ({self.description}) ({self.picture}) ({self.price})"
+        return f"({self.Pcode}) ({self.Ccode}) ({self.description}) ({self.picture}) ({self.price})"
+
+
+class Categories(models.Model):
+    Ccode=models.IntegerField()
+    description = models.CharField(max_length=50)
+    def __str__(self):
+        return f"({self.Ccode}) ({self.description})"
+
+
 
 #Auctions List
 class Listings(models.Model):
     Lcode= models.IntegerField() #Key?
-    user = models.CharField(help_text="User responsable for the Listings",max_length=25)
+    Pcode = models.CharField(max_length=5) #foreignkey Property
     dateStart = models.DateField(help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
     duration = models.IntegerField(help_text="Duration expressed in days" )
-    Pcode = models.CharField(max_length=5) #foreignkey Property
     status = models.CharField(max_length=5) #to Begin, on , ended
     def __str__(self):
-        return f"({self.lcode}) ({self.user}) ({self.dateStart}) ({self.dateEnd }) ({self.duration}) ({self.Pcode})({self.status})"
+        return f"({self.lcode})({self.Pcode}) ({self.dateStart}) ({self.duration}) ({self.status})"
 
 #bids list
 class Bids(models.Model):
@@ -40,11 +47,22 @@ class Bids(models.Model):
 #Comments
 class Comments(models.Model):
     Comcode =models.IntegerField() #Key
-    Listcode= models.IntegerField() #foreign key
+    Lcode= models.IntegerField() #foreign key
     user = models.CharField(max_length=25)
     comment = models.CharField(max_length=250)
     def __str__(self):
         return f"({self.Comcode}) ({self.Listcode}) ({self.user}) ({self.comment})"
+
+
+#Like Faforits???
+class Watchlist(models.Model):
+    code =models.IntegerField() #Key
+    user = models.CharField(max_length=25)
+    Lcode= models.IntegerField()
+
+
+
+
 #Auction Results - Vendido para...
 
 class Soldto(models.Model):
@@ -55,4 +73,4 @@ class Soldto(models.Model):
     bidprice = models.DecimalField(max_digits=9, decimal_places=2)
     status = models.CharField(max_length=5) #open or closed
     def __str__(self):
-    return f"({self.Lcode})({self.Pcode}) ({self.user}) ({self.date})  ({self.bidprice})  ({self.status})"
+        return f"({self.Lcode})({self.Pcode}) ({self.user}) ({self.date})  ({self.bidprice})  ({self.status})"
