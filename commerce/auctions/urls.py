cc9de1447a5729a6b1d3450540f1e45acb75ede8
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-14 20:04:09 -0300

    Update urls.py

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("property", views.property, name="property"),
+    path("bids", views.bids, name="bids"),
+    path("listings", views.listings, name="listings"),
+    path("comments", views.comments, name="comments"),
+    path("soldto", views.soldto, name="soldto"),
+
 ]
