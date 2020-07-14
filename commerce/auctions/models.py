aad3f6a398558d2e48d9c63621a842c73cb7abdd
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-14 16:18:36 -0300

    Update models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -5,10 +5,21 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Models(models.Model):
+class Items(models.Model):
     code = models.CharField(max_length=5)
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=60)
-    price = models.Numeric()
-    finalprice = models.Numeric()
-    picture = models.CharField(max_length=60)
+    picture = models.CharField(max_length=60) #Name of the picture saved on static folder
+    price = models.DecimalField()
+    finalprice = models.DecimalFieldc()
+    def __str__(self):
+        return f"{self.code} ({self.name}) ({self.description}) ({self.picture}) ({self.price}) ({self.finalprice}) "
+
+class Bids(models.Model):
+    code = models.CharField(max_length=5)  #item code, the same as in Items
+    user = models.CharField(max_length=25)
+    userbid = models.DecimalField()
+    def __str__(self):
+        return f"{self.code} ({self.user}) ({self.userbid })"
+
+        
