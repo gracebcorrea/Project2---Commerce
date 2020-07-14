461743214263083d8c13666f2291c7a9a18a3984
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-14 15:48:03 -0300

    Update models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Models(models.Model):
+    code = models.CharField(max_length=5)
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=60)
+    price = models.Numeric()
+    finalprice = models.Numeric()
+    picture = models.CharField(max_length=60)
