bf25f06525fcb22ad0e3df7c2851cdb1959ca50c
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-15 15:31:09 -0300

    Update models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,56 +6,57 @@ from PIL.Image import core as _imaging
 class User(AbstractUser):
     pass
 
-#Properties  register
+#Properties   table  auctions_ActiveListings
 class ActiveListings(models.Model):
-    Pcode = models.CharField(max_length=5) #key
-    Ccode=models.IntegerField(help_text="Category House, Apartament, Commercial") #foreign key from PropTypes
+    Pcode = models.IntegerField(max_length=5) #key
+    Ccode= models.IntegerField(help_text="Category House, Apartament, Commercial") #foreign key from PropTypes
     description = models.CharField(max_length=250)
     picture = models.ImageField() #Name of the picture saved on static folder
     price = models.DecimalField(help_text="Just USD", max_digits=9, decimal_places=2) #initial price
     def __str__(self):
         return f"({self.Pcode}) ({self.Ccode}) ({self.description}) ({self.picture}) ({self.price})"
 
-
+#Properties types table auctions_Categories
 class Categories(models.Model):
-    Ccode=models.IntegerField()
+    Ccode=models.IntegerField() #key
     description = models.CharField(max_length=50)
     def __str__(self):
         return f"({self.Ccode}) ({self.description})"
 
 
 
-#Auctions List
+#Auctions List  table auctions_Listings
 class Listings(models.Model):
     Lcode= models.IntegerField() #Key?
-    Pcode = models.CharField(max_length=5) #foreignkey Property
+    Pcode = models.IntegerField(max_length=5) #foreignkey Property
     dateStart = models.DateField(help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
     duration = models.IntegerField(help_text="Duration expressed in days" )
     status = models.CharField(max_length=5) #to Begin, on , ended
     def __str__(self):
         return f"({self.lcode})({self.Pcode}) ({self.dateStart}) ({self.duration}) ({self.status})"
 
-#bids list
+#bids list  table auctions_Bids
 class Bids(models.Model):
+    Bcode = models.IntegerField() #pk
     Lcode= models.IntegerField() #foreignkey Listings
-    Pcode = models.CharField(max_length=5)  #foreignkey Property
+    Pcode = models.IntegerField(max_length=5)  #foreignkey Property
     user = models.CharField(max_length=25) #Who is going to buy
-    bidnumber= models.IntegerField() #first throw, second , third....
-    bidprice = models.DecimalField(max_digits=9, decimal_places=2)# throw value
+    Bthrow= models.IntegerField() #first throw, second , third....
+    Bprice = models.DecimalField(max_digits=9, decimal_places=2)# throw value
     def __str__(self):
-        return f"({self.Lcode})({self.Pcode}) ({self.user}) ({self.bidnumber})  ({self.bidprice})"
+        return f"({self.Bcode})({self.Lcode})({self.Pcode}) ({self.user}) ({self.Bthrow})  ({self.Bprice})"
 
-#Comments
+#Comments table auctions_Comments
 class Comments(models.Model):
-    Comcode =models.IntegerField() #Key
+    Ccode =models.IntegerField() #Key
     Lcode= models.IntegerField() #foreign key
     user = models.CharField(max_length=25)
     comment = models.CharField(max_length=250)
     def __str__(self):
-        return f"({self.Comcode}) ({self.Lcode}) ({self.user}) ({self.comment})"
+        return f"({self.Ccode}) ({self.Lcode}) ({self.user}) ({self.comment})"
 
 
-#Like Faforits???
+#Like Faforits???  table auctions_Watchlist
 class Watchlist(models.Model):
     Wcode =models.IntegerField() #Key
     Lcode= models.IntegerField()
@@ -64,14 +65,15 @@ class Watchlist(models.Model):
         return f"({self.Wcode}) ({self.Lcode}) ({self.user}) "
 
 
-#Auction Results - Vendido para...
+#Auction Results - Vendido para...  table auctions_Soldto
 
 class Soldto(models.Model):
+    Scode=models.IntegerField()
     Lcode= models.IntegerField()
-    Pcode = models.CharField(max_length=5)
+    Pcode = models.IntegerField(max_length=5)
     user  = models.CharField(max_length=25)
     date = models.DateField(help_text="Please use the following format: <em>YYYY-MM-DD</em>.")
-    bidprice = models.DecimalField(max_digits=9, decimal_places=2)
+    Bprice = models.DecimalField(max_digits=9, decimal_places=2)
     status = models.CharField(max_length=5) #open or closed
     def __str__(self):
-        return f"({self.Lcode})({self.Pcode}) ({self.user}) ({self.date})  ({self.bidprice})  ({self.status})"
+        return f"({self.Lcode})({self.Pcode}) ({self.user}) ({self.date})  ({self.Bprice})  ({self.status})"
