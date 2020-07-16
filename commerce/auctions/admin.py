5220c98d63ebe0e90fa55084c09aa56de018f0f8
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-16 11:20:08 -0300

    Update admin.py

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+#from django.contrib import admin
+
+from .models import Listings, Categories, ActiveListings, Bids, Comments, Watchlist,SoldTo
 # Register your models here.
+
+#class FlightAdmin(admin.ModelAdmin):
+#    list_display = ("__str__", "duration")
+
+class Admin(admin.ModelAdmin):
+    filter_horizontal = ("comerce",)
+
+
+admin.site.register(Listings)
+admin.site.register(Categories)
+admin.site.register(ActiveListings)
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(Watchlist)
+admin.site.register(SoldTo)
+
+#admin.site.register(Passenger, PassengerAdmin)
