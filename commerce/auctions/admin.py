34cda5f88d149eb87bce8bc6086aefad14724b8a
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-17 18:35:30 -0300

    Update admin.py

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -2,17 +2,16 @@ from django.contrib import admin
 
 #from django.contrib import admin
 
-from .models import Listings, Categories, ActiveListings, Bids, Comments, Watchlist
+from .models import Listings, Categories, Bids, Comments, Watchlist
 # Register your models here.
 
 
 class Admin(admin.ModelAdmin):
-    filter_horizontal = ("comerce",)
+    filter_horizontal = ("commerce",)
 
 
 admin.site.register(Listings)
 admin.site.register(Categories)
-admin.site.register(ActiveListings)
 admin.site.register(Bids)
 admin.site.register(Comments)
 admin.site.register(Watchlist)
