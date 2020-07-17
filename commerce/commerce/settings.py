20817e8475ca6d148c204fa2e7ec096b63d187b5
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-16 23:06:07 -0300

    Update settings.py

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -121,6 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-#MEDIA_URL = '/images/'
-#MEDIA_ROUT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/static/images/'
+MEDIA_ROUT = os.path.join(BASE_DIR, 'static/images')
