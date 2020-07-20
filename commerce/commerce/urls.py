262b7d21f0983decd31dc61f2d19a363d987d57b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-20 15:01:07 -0300

    Update urls.py

--- commerce/commerce/urls.py
+++ commerce/commerce/urls.py
@@ -21,6 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls", namespace="auctions"))
-]
-
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
