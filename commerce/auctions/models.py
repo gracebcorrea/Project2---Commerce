97d81e49a6d1a96acf094a32074c741f49a8151b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-21 13:20:07 -0300

    Update models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -41,8 +41,8 @@ class Listings(models.Model):
     Limage= models.ImageField(upload_to="media", blank=True) #Name of the picture saved on static folder
     Lstatus = models.CharField(max_length=8, choices=STATUS )
     def __str__(self):
-        return f"({self.Ltitle}) ({self.Ccode}) ({self.Ldescription}) ({self.Lprice}) ({self.Ldatestart}) ({self.Lduration}) ({self.Luser})({self.Limage})({self.Lstatus}) "
-
+        return f"({self.Ltitle}) ({self.Lstatus}) "
+#({self.Ccode}) ({self.Ldescription}) ({self.Lprice}) ({self.Ldatestart}) ({self.Lduration}) ({self.Luser})({self.Limage})
 #Properties types table auctions_Categories
 class Categories(models.Model):
     Ccode=models.IntegerField() #key
