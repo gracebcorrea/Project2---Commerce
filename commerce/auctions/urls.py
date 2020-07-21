45beb6cceff90f58ce80872f9a9cb0556e85bce5
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-20 23:54:16 -0300

    Update urls.py

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 from . import views
 from .views import index, login_view,logout_view,register
-from .views import CreateListings_view,Listings_view,Categories_view,Comments_view,Watchlist_view,Bids_view
-
+from .views import CreateListings_view, Listingspage_view, Comments_view
+from .views import Watchlist_view, Categories_view, CategoryShow_view
 
 
 app_name = 'auctions'
@@ -16,15 +16,15 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
 
-
     path("index", views.index, name="index"),
     path("CreateListings", views.CreateListings_view, name="CreateListings"),
-    path("Categories", views.Categories_view, name="Categories"),
-    path("<str:C_description>", views.CategoryShow_view, name="CategoryShow"),
-    path("Listings", views.Listings_view, name="Listings"),
-    path("Bids", views.Bids_view, name="Bids"),
+
+    path("Listingspage", views.Listingspage_view, name="Listingspage"),
     path("Comments", views.Comments_view, name="Comments"), #vinculado a bids
+
     path("Watchlist", views.Watchlist_view, name="Watchlist"),
 
+    path("Categories", views.Categories_view, name="Categories"),
+    path("<str:C_description>", views.CategoryShow_view, name="CategoryShow"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
