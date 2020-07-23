558b9a2c577563e29bd9de798ddff6bbb1ca61d0
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-23 20:46:19 -0300

    Update urls.py

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from . import views
 from .views import index, login_view,logout_view,register
 from .views import CreateListings_view, Listingspage_view, Comments_view, Bids_view
-from .views import Watchlist_view, Categories_view, CategoryShow_view
+from .views import Watchlist_view, Categories_view, CategoryShow_view, Bid_add
 
 
 app_name = 'auctions'
@@ -19,13 +19,13 @@ urlpatterns = [
     path("index", views.index, name="index"),
     path("CreateListings", views.CreateListings_view, name="CreateListings"),
 
-
     path("Listingspage", views.Listingspage_view, name="Listingspage"),
     path("Bid/<str:Btitle>", views.Bids_view, name="BidsDetail"),
+    path("Bid/<str:Btitle>", views.Bid_add, name="BidsDetail"),
+
     path("Comments", views.Comments_view, name="Comments"), #vinculado a bids
 
     path("Watchlist", views.Watchlist_view, name="Watchlist"),
-
     path("Categories", views.Categories_view, name="Categories"),
     path("Category/<str:C_description>", views.CategoryShow_view, name="CategoryShow"),
 
