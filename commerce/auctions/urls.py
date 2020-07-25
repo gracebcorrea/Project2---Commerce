698833667c9d7fa2caf35ff884a22c9533de87e3
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-24 23:44:20 -0300

    Update urls.py

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -11,21 +11,21 @@ from .views import Watchlist_view, Categories_view, CategoryShow_view
 
 app_name = 'auctions'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
 
-    path("index", views.index, name="index"),
-    path("CreateListings", views.CreateListings_view, name="CreateListings"),
+    path('index', views.index, name='index'),
+    path('CreateListings', views.CreateListings_view, name='CreateListings'),
 
-    path("Listingspage", views.Listingspage_view, name="Listingspage"),
-    path("Bid/<str:Btitle>", views.Bids_view, name="BidsDetail"),
+    path('Listingspage', views.Listingspage_view, name='Listingspage'),
+    path('Bid/<str:Btitle>', views.Bids_view, name='BidsDetail'),
 
-    path("Comments", views.Comments_view, name="Comments"), #vinculado a bids
+    path('Comments', views.Comments_view, name='Comments'), #vinculado a bids
 
-    path("Watchlist", views.Watchlist_view, name="Watchlist"),
-    path("Categories", views.Categories_view, name="Categories"),
-    path("Category/<str:C_description>", views.CategoryShow_view, name="CategoryShow"),
+    path('Watchlist', views.Watchlist_view, name='Watchlist'),
+    path('Categories', views.Categories_view, name='Categories'),
+    path('Category/<str:C_description>', views.CategoryShow_view, name='CategoryShow'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
