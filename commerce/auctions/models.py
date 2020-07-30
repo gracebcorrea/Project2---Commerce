20dc55771ea779dc46646168463c3689f565045c
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-07-30 12:28:51 -0300

    Update models.py

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -15,13 +15,6 @@ class User(AbstractUser):
     pass
 
 
-
-"""
-Models: Your application should have at least three models in addition to the User model:
-one for auction listings, one for bids, and one for comments made on auction listings.
-It’s up to you to decide what fields each model should have, and what the types of those
-fields should be. You may have additional models if you would like.
-"""
 #Model 1   auctions_Listings
 class Listings(models.Model):
     STATUS = (
